6f4eb481a5d12fa2bc0278d09dd3522fa740e347
Andrew Nam <w7jh9a2@gmail.com>
2018-07-30 21:54:21 -0700

    FlickrBrowser complete

---
+++ app/src/main/java/org/andrewnam/flickrbrowser/BaseActivity.java
@@ -0,0 +1,35 @@
+package org.andrewnam.flickrbrowser;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+
+
+/**
+ * Created by Nam on 3/15/2018.
+ */
+
+public class BaseActivity extends AppCompatActivity{
+    private static final String TAG = "BaseActivity";
+    static final String FLICKR_QUERY = "FLICKR_QUERY";
+    static final String PHOTO_TRANSFER = "PHOTO_TRANSFER";
+
+    void activateToolbar(boolean enableHome) {
+        Log.d(TAG, "activateToolbar: starts");
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar == null) {
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+            if (toolbar != null) {
+                setSupportActionBar(toolbar);
+                actionBar = getSupportActionBar();
+            }
+        }
+
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(enableHome);
+        }
+    }
+
+}
